b35028a11e9566931d15907e3292a96467fd4ef4
kmp <jemzetrim@gmail.com>
2017-09-20 23:43:11 +0400

    feat:  - modal-card markup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import $ from 'jquery';
 import './css/index.pcss';
-import './js/lamp-slider-theme.pcss';
-import './js/lamp-slider.pcss';
+import './js/lamp-slider/lamp-slider-theme.pcss';
+import './js/lamp-slider/lamp-slider.pcss';
 
-import initLampSlick from './js/lamp-slider';
+import initLampSlick from './js/lamp-slider/lamp-slider';
+import initCardSlick from './js/card-slider/card-slider';
 
 window.addEventListener('load', () => {
     const LampSliderSelector = $('.lamp-slider');
     initLampSlick(LampSliderSelector);
+
+    const CardSliderSelector = $('.card-slider');
+    initCardSlick(CardSliderSelector);
 });
 
