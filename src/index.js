dfcfef322e53d5bddb5992f6798a0ffa69951bff
kmp <jemzetrim@gmail.com>
2017-09-21 23:21:16 +0400

    feat:  - function getCurrentImage  - some css fixes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import $ from 'jquery';
 import './css/index.pcss';
 import './js/lamp-slider/lamp-slider-theme.pcss';
 import './js/lamp-slider/lamp-slider.pcss';
+import './js/card-slider/card-slider-theme.pcss';
 
 import initLampSlick from './js/lamp-slider/lamp-slider';
 import initCardSlick from './js/card-slider/card-slider';
+
+import getCurrentImage from './js/getCurrImage';
 
 window.addEventListener('load', () => {
     const LampSliderSelector = $('.lamp-slider');
@@ -12,5 +15,12 @@ window.addEventListener('load', () => {
 
     const CardSliderSelector = $('.card-slider');
     initCardSlick(CardSliderSelector);
+
+    const getCurrentImageOptions = {
+        largeImageSelector: document.getElementsByClassName('card__large-image')[0],
+        smallImagesSelector: document.getElementsByClassName('card__small-image')
+    };
+
+    getCurrentImage(getCurrentImageOptions);
 });
 
