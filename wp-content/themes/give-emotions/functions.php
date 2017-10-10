cc986e11fd431e63c8428ef25c1476e76d0c204f
zmv <grizz8575@gmail.com>
2017-10-10 22:31:01 +0400

    первый метод апи
    (cherry picked from commit 7ef17e1)

--- wp-content/themes/give-emotions/functions.php
+++ wp-content/themes/give-emotions/functions.php
@@ -8,38 +8,49 @@
 add_action( 'wp_enqueue_scripts', 'my_assets' );
 add_action( 'wp_head', 'js_variables' );
 
-if ( wp_doing_ajax () ) {
+if ( wp_doing_ajax() ) {
 	add_action( 'wp_ajax_get_post_info', 'getPostInfo' );
 	add_action( 'wp_ajax_nopriv_get_post_info', 'getPostInfo' );
 }
 
 function my_assets() {
-	wp_register_script( 'index_js', get_stylesheet_directory_uri() . '/front/build/index.js',null, null, true );
+	wp_register_script( 'index_js', get_stylesheet_directory_uri() . '/front/build/index.js', null, null, true );
 	wp_enqueue_script( 'index_js' );
 
 	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/front/build/index.css' );
 }
+
 function js_variables() {
 	$variables = array(
 		'ajax_url' => admin_url( 'admin-ajax.php' ),
 	);
-    echo('<script type="text/javascript">window.wp_data = ' . json_encode($variables) . ';</script>');
+	echo( '<script type="text/javascript">window.wp_data = ' . json_encode( $variables ) . ';</script>' );
 }
 
-function getPostInfo(){
+function getPostInfo() {
 	$postID = $_POST['postId'];
-	if ($postID){
-		$product = get_post($postID);
+	if ( $postID ) {
+		$product = get_post( $postID );
 
-		$gallery = get_post_gallery_images($postID);
-		$result = [
-			'gallery' => $gallery,
+		$gallery = get_post_gallery_images( $postID );
+		$result  = [
+			'gallery'     => $gallery,
 			'productInfo' => $product
 		];
-	}else{
-		$result = ['error' => 'Can\'t find product with id ' . $postID];
+	} else {
+		$result = [ 'error' => 'Can\'t find product with id ' . $postID ];
 	}
-
-	echo json_encode($result);
+	sendJsonResponse( $result );
 	wp_die();
+}
+
+/**
+ * @param $data
+ */
+function sendJsonResponse( $data ) {
+	if ( $data['error'] ) {
+		http_response_code( 500 );
+	}
+	header( 'Content-Type: application/json' );
+	echo json_encode( $data );
 }
